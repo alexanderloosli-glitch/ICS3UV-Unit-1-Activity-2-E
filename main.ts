f3bfe1a9a103f70b68b304ad974dc2ca4c8866c9
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-23 16:26:48 +0000

    added 34.5 * 2 + 6.9 = 75.9

--- main.ts
+++ main.ts
@@ -5,7 +5,13 @@
  * @fileoverview This program shows code doing math.
  */
 
-// Note: The use of "string" to convert a number to a sting
-console.log("34.5 + 34.5 + 6.9 = " + string(34.5 + 34.5 + 6.9));
+// Note: The use of "string" to convert a number to a string
+console.log("34.5 + 34.5 = " + String(34.5 + 34.5));
+
+// Note: the use of "string" to convert a number to a string
+console.log("69 / 10 = " + String(69 / 10));
+
+// Note the use of "sring" to conver a number to a string 
+console.log("69 + 6.9 = " + String(69 + 6.9));
 
 console.log("\nDone");
