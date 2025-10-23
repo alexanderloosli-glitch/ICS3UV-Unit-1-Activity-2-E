388fd4861ab3eef1414edaf649869cf336e999be
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-23 16:10:11 +0000

    added file 34.5 * 2 + 10%ANS

---
+++ main.ts
@@ -0,0 +1,16 @@
+/**
+ * @author mr loosli
+ * @version 1.0.0
+ * @date 2025-10-21
+ * @fileoverview This program practices with string literals.
+ */
+
+// string literal text
+//this displays the sentence The product 
+console.log("The product of 34.5 and 34.5 and 6.9 is equal to "+75.9
+  // string literal text
+  //console.log("the product of 34.5 and 34.5 and 6.9 is equal to " + (34.5 + 34.5 + 6.9));
+
+  //this displays 34.5 * 2 + 10%ANS = 75.9
+  //console.log("34.5 * 2 + 10%ANS = " + (34.5 * 2 + 10%ANS));
+
