23ddb1a508ec66ff0079524401e5b1ce24caff58
alex-gordon <alexander.loosli@stu.ocsb.ca>
2025-10-23 16:18:37 +0000

    added file 34.5 + 34.5 + 6.9

--- main.ts
+++ main.ts
@@ -2,15 +2,10 @@
  * @author mr loosli
  * @version 1.0.0
  * @date 2025-10-21
- * @fileoverview This program practices with string literals.
+ * @fileoverview This program shows code doing math.
  */
 
-// string literal text
-//this displays the sentence The product 
-console.log("The product of 34.5 and 34.5 and 6.9 is equal to "+75.9
-  // string literal text
-  //console.log("the product of 34.5 and 34.5 and 6.9 is equal to " + (34.5 + 34.5 + 6.9));
+// Note: The use of "string" to convert a number to a sting
+console.log("34.5 + 34.5 + 6.9 = " + string(34.5 + 34.5 + 6.9));
 
-  //this displays 34.5 * 2 + 10%ANS = 75.9
-  //console.log("34.5 * 2 + 10%ANS = " + (34.5 * 2 + 10%ANS));
-
+console.log("\nDone");
